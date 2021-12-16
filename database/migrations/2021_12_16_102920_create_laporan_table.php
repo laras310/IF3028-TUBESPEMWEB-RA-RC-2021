93d17c2320b7e32c030011cfaa5a9a60a0525ee8
mayonice1424 <friyandrailham14@gmail.com>
2021-12-16 17:30:12 +0700

    Membuat database

---
+++ database/migrations/2021_12_16_102920_create_laporan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaporanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama',50);
+            $table->string('pesan',1000);
+            $table->string('aspek',20);
+            $table->text('file');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporan');
+    }
+}
